debf9b6de2098c99b8b72c2554f581fdc002ea58
mc-b <marcel.bernet@ch-open.ch>
2022-04-13 13:31:31 +0200

    Provider in sep. Datei verschoben - flexibler!

--- main.tf
+++ main.tf
@@ -1,18 +1,6 @@
-terraform {
-  required_providers {
-    maas = {
-      source = "suchpuppet/maas"
-      version = "3.1.3"
-    }
-  }
-}
-
-provider "maas" {
-    # Configuration options
-    api_version = "2.0"
-    api_key = "nC83nVyLDWKF8zxvPq:VnYukvR9Yh3w9jRDUe:FdU7sFWJu8DHjAeRumRNmZfasNBDqgXa"
-    api_url = "http://10.6.37.8:5240/MAAS"
-}
+###
+#   Ressourcen
+#
 
 resource "maas_instance" "vm" {
     deploy_hostname = var.module
